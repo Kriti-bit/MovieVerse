ca55b2fb15e320cf0a5e914ed9126ad7169321a9
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-08-01 17:15:13 +0530

    Updates

---
+++ precache-manifest.1e26c3a14df441455321489c3aaaa5cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1564e97dc7c565cb6978dcb4fc82f22e",
+    "url": "/MovieVerse/index.html"
+  },
+  {
+    "revision": "5e7363411c6407e66342",
+    "url": "/MovieVerse/static/css/main.28c49d61.chunk.css"
+  },
+  {
+    "revision": "7fb00b82c4de3d79c865",
+    "url": "/MovieVerse/static/js/2.4b3567bd.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/MovieVerse/static/js/2.4b3567bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7363411c6407e66342",
+    "url": "/MovieVerse/static/js/main.f969e13e.chunk.js"
+  },
+  {
+    "revision": "0163b529f63b15e7a6f0",
+    "url": "/MovieVerse/static/js/runtime-main.70238d0a.js"
+  }
+]);
